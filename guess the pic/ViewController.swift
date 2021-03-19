6bbc0f432efcacdc1c06e5aa0589ce569e677aea
sebmaz <titan9389@me.com>
2021-03-19 08:39:06 +0400

    init

---
+++ guess the pic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  guess the pic
+//
+//  Created by Sebouh Mazloumian on 19.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
